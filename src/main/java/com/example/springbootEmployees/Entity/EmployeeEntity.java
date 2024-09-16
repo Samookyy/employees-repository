a5032b9e43898b00f379d7a2694c933ede8583c5
Samookyy <muhammad.suleim@gmail.com>
2024-09-16 15:04:36 +0200

    columms added

--- src/main/java/com/example/springbootEmployees/Entity/EmployeeEntity.java
+++ src/main/java/com/example/springbootEmployees/Entity/EmployeeEntity.java
@@ -1,31 +1,50 @@
 package com.example.springbootEmployees.Entity;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
+
 
 @Entity
+@Table(name = "employeeDB")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long employeeId;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "surname")
     private String surname;
-    private String dateOfBirth;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "civil_status")
     private String civilStatus;
+
+    @Column(name = "id_document")
     private String idDocument;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "email")
     private String email;
 }
