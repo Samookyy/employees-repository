7cb7dcdc1fd6f2ed4a1518e15266593bda7872ff
Samookyy <muhammad.suleim@gmail.com>
2024-09-18 14:56:54 +0200

    Updated sevice impl

--- src/main/java/com/example/springbootEmployees/service/Employee_Service_impl.java
+++ src/main/java/com/example/springbootEmployees/service/Employee_Service_impl.java
@@ -49,11 +49,27 @@ public class Employee_Service_impl implements Employee_Service {
 
     @Override
     public EmployeeEntity updateEmployeeById(Long id, EmployeeRecordDto employeeRecordDto) {
-        return null;
+        EmployeeEntity employeeEntity = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee Not Found"));
+
+        employeeEntity.setName(employeeRecordDto.name());
+        employeeEntity.setSurname(employeeRecordDto.surname());
+        employeeEntity.setDateOfBirth(employeeRecordDto.dateOfBirth());
+        employeeEntity.setCivilStatus(employeeRecordDto.civilStatus());
+        employeeEntity.setIdDocument(employeeRecordDto.idDocument());
+        employeeEntity.setNuit(employeeRecordDto.nuit());
+        employeeEntity.setAddress(employeeRecordDto.address());
+        employeeEntity.setPhoneNumber(employeeRecordDto.phoneNumber());
+        employeeEntity.setEmail(employeeRecordDto.email());
+
+        return employeeRepository.save(employeeEntity);
     }
 
     @Override
     public void deleteEmployeeById(Long id) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee Not Found"));
 
+        employeeRepository.delete(employeeEntity);
     }
 }
